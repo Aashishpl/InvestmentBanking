292ee1e3cf87c685d91f94b134d0543bb9d6b68d
Aashishpl <110179632+Aashishpl@users.noreply.github.com>
2022-10-03 00:14:13 +0530

    newsysoutadded

--- SampleProject/src/myPackage/TestBase.java
+++ SampleProject/src/myPackage/TestBase.java
@@ -5,6 +5,7 @@ public class TestBase {
 	public static void main(String[] args) {
 		System.out.println("Common properties");
 		System.out.println("Common properties");
+		System.out.println("Changes made");
 	}
 
 }
